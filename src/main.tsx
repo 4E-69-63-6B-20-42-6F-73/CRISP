9708ebf1832bf8db592280905df9d2bae7d8d14a
Nick Bos <Nick.Bos@studiemaatje.company>
2024-09-19 17:25:04 +0200

    Set basename on router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Routes } from '@generouted/react-router'
+import { routes } from '@generouted/react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './main.css'
 
+const router = createBrowserRouter(routes, { basename:"/CRISP/" })
+
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
-      <Routes/>  
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
 
