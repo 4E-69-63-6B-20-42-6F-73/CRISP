11c76ac7bd987c411b46c1f5ca08bde7aa1bd395
Nick <20791144+4E-69-63-6B-20-42-6F-73@users.noreply.github.com>
2024-09-20 14:22:18 +0200

    Add ui to determine progress on predicting

--- src/pages/predict/[id].tsx
+++ src/pages/predict/[id].tsx
@@ -1,21 +1,97 @@
-import { useParams, useNavigate } from "../../router";
-import {
-  useAddPrediction,
-  useGetAnalysesById,
-} from "../../stores/ApplicationStore";
+import { useEffect, useState } from "react";
+import { ProgressBar } from "@fluentui/react-components";
 
-export default function Details() {
+import {
+  useGetAnalysesById,
+  useAddPrediction,
+} from "../../stores/ApplicationStore";
+import { useNavigate, useParams } from "../../router";
+
+export default function Predict() {
   const params = useParams("/predict/:id");
   const id = Number(params.id);
 
   const navigate = useNavigate();
 
-  const analyse = useGetAnalysesById(id);
   const addPrediction = useAddPrediction();
 
-  addPrediction(id, [{ patientId: "123", prediction: 1 }]);
+  const [state, setState] = useState<State>("extracting");
+  const [predicted] = useState(0);
+  const text = getText(state, predicted, 200);
+
+  useEffect(() => {
+    const simulatePrediction = async () => {
+      for await (const newState of predictor()) {
+        setState(newState);
+      }
+    };
+
+    simulatePrediction();
+  }, []);
+
+  const analyse = useGetAnalysesById(id);
+
+  addPrediction(analyse.id, [{ patientId: "123", prediction: 1 }]);
 
   navigate("/detail/:id", { params: { id: id.toString() } });
 
-  return <h1>Predicting... {analyse.id}</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "flex-end",
+      }}
+    >
+      <div
+        style={{
+          minHeight: "200px",
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "flex-end",
+          flexDirection: "column",
+        }}
+      >
+        <h3>{text.line3}</h3>
+        <h2>{text.line2}</h2>
+        <h1>{text.line1}</h1>
+      </div>
+
+      <ProgressBar style={{ width: "50%" }} />
+      <p>Please do not close this window</p>
+    </div>
+  );
+}
+
+type State = "extracting" | "loading" | "predicting";
+
+const getText = (state: State, predicted: number, total: number) => {
+  switch (state) {
+    case "extracting":
+      return { line1: "Extracting data", line2: "", line3: "" };
+    case "loading":
+      return { line1: "Loading model", line2: "Extracting data", line3: "" };
+    case "predicting":
+      return {
+        line1: `Predicting ${predicted + 1} / ${total}`,
+        line2: "Loading model",
+        line3: "Extracting data",
+      };
+    default:
+      return { line1: "", line2: "", line3: "" };
+  }
+};
+
+async function* predictor() {
+  yield new Promise<State>((resolve) =>
+    setTimeout(() => resolve("extracting"), 500)
+  );
+
+  yield new Promise<State>((resolve) =>
+    setTimeout(() => resolve("loading"), 1000)
+  );
+  yield new Promise<State>((resolve) =>
+    setTimeout(() => resolve("predicting"), 2000)
+  );
 }
