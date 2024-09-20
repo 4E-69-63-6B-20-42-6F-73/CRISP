7626632f68039be768eb87c40407b04e40a8dba9
Nick Bos <Nick.Bos@studiemaatje.company>
2024-09-20 11:36:58 +0200

    Added grid in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,58 @@
-import { Breadcrumb, BreadcrumbButton, BreadcrumbItem, Button, makeStyles } from "@fluentui/react-components";
-import { AddFilled} from "@fluentui/react-icons";
+import { Breadcrumb, BreadcrumbButton, BreadcrumbItem, Button, makeStyles, tokens } from "@fluentui/react-components";
+import { AddFilled } from "@fluentui/react-icons";
 import { useNavigate } from "../router";
+import { useAnalyses } from "../stores/ApplicationStore";
+import AnalyseDataGrid from "../components/index/analysesDatagrid";
 
 
 const useClasses = makeStyles({
-    div:{
-        display:"flex",
-        gap:"12px"
+    div: {
+        display: "flex",
+        gap: "12px"
     },
     button: {
         background: "linear-gradient(128.84deg,#0f6cbd 20.46%,#3c45ab 72.3%)", //TODO use tokens??
-        ":hover":{
+        ":hover": {
             background: "linear-gradient(128.84deg, #025caa 20.46%, #222b91 72.3%);"
         }
     },
+    datagrid: {
+        maxWidth: "1000px",
+        width: "-webkit-fill-available",
+        background: tokens.colorNeutralBackground3,
+        borderRadius: tokens.borderRadiusLarge,
+        padding: "12px",
+        marginTop: "12px",
+    }
 })
 
 export default function Index() {
     const classes = useClasses()
     const navigate = useNavigate()
+    const analyses = useAnalyses()
 
-    return <div className={classes.div}>
-        <Breadcrumb>
-            <BreadcrumbItem>
-                <BreadcrumbButton current>My analyses</BreadcrumbButton>
-            </BreadcrumbItem>
-        </Breadcrumb>
-        <Button className={classes.button} appearance="primary" shape="circular" icon={<AddFilled />} onClick={() => navigate("/new")}> Create new</Button>
-    </div>
+    return <>
+        <div className={classes.div}>
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <BreadcrumbButton current>My analyses</BreadcrumbButton>
+                </BreadcrumbItem>
+            </Breadcrumb>
+            <Button className={classes.button} appearance="primary" shape="circular" icon={<AddFilled />} onClick={() => navigate("/new")}> Create new</Button>
+        </div>
+
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+        }}>
+  <div className={classes.datagrid}>
+            <AnalyseDataGrid items={analyses} />
+        </div>
+        </div>
+      
+
+
+
+
+    </>
 }
