231fb337eb6dbe468b2888f1a995abf0a3972893
Nick Bos <Nick.Bos@studiemaatje.company>
2024-09-20 11:36:50 +0200

    Added zustand and applicationstore

---
+++ src/stores/ApplicationStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand'
+
+import { Analyse } from '../types';
+
+interface ApplicationState {
+    analyses: Analyse[]
+    addAnalyse: (analyse: Analyse) => void
+}
+
+const useApplicationStore = create<ApplicationState>()((set) => ({
+    analyses: [
+        {
+            name: "Test",
+            description: "",
+            created: new Date(2021, 4, 27),
+            files:[]
+        },
+        {
+            name: "Test2",
+            description: "",
+            created: new Date(2023, 5, 13),
+            files:[]
+        },
+        {
+            name: "Test3",
+            description: "gfdgsdfg",
+            created: new Date(2023, 4, 5),
+            files:[]
+        },
+    ],
+    addAnalyse: (analyse: Analyse) =>  set((state) => ({ analyses: [...state.analyses, analyse] })),
+}))
+
+
+const useAnalyses = () => useApplicationStore(s => s.analyses)
+
+export { useAnalyses }
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+type Analyse = {
+    name: string;
+    description: string;
+    created: Date
+    files: File[];
+};
+
+export type { Analyse };
