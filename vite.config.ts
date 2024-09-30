d563234737df5eae8f575ff58caca64781c996cd
Nick Bos <Nick.Bos@studiemaatje.company>
2024-09-30 10:19:27 +0200

    Added alias to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import generouted from '@generouted/react-router/plugin'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/CRISP/",
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+
+  },
+  base: "/CRISP/",
   plugins: [react(), generouted()],
-  
+
 })
+
