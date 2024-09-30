b29cae1e838c3aaa408b9a47277da1747127e995
Nick Bos <Nick.Bos@studiemaatje.company>
2024-09-30 11:33:22 +0200

    Persist Store in sessionStorage

--- src/stores/ApplicationStore.ts
+++ src/stores/ApplicationStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 import { Analyse, Prediction } from "../types";
 
@@ -8,66 +9,88 @@ interface ApplicationState {
   addPrediction: (id: number, predictions: Prediction[]) => void;
 }
 
-const useApplicationStore = create<ApplicationState>()((set) => ({
-  analyses: [
-    {
-      id: 1,
-      name: "Test",
-      description: "",
-      created: new Date(2021, 4, 27),
-      files: [],
-      prediction: [
-        { patientId: "1", prediction: 1 },
-        { patientId: "2", prediction: 2 },
-        { patientId: "3", prediction: 3 },
-        { patientId: "4", prediction: 4 },
+const useApplicationStore = create<ApplicationState>()(
+  persist(
+    (set) => ({
+      analyses: [
+        {
+          id: 1,
+          name: "Test",
+          description: "",
+          created: new Date(2021, 4, 27),
+          files: [],
+          prediction: [
+            { patientId: "1", prediction: 1 },
+            { patientId: "2", prediction: 2 },
+            { patientId: "3", prediction: 3 },
+            { patientId: "4", prediction: 4 },
+          ],
+        },
+        {
+          id: 2,
+          name: "Test2",
+          description: "",
+          created: new Date(2023, 5, 13),
+          files: [],
+        },
+        {
+          id: 3,
+          name: "Test3",
+          description: "gfdgsdfg",
+          created: new Date(2023, 4, 5),
+          files: [],
+        },
       ],
-    },
+
+      addAnalyse: (analyse: Omit<Analyse, "id">) => {
+        let newId: number = 0;
+        set((state) => {
+          const maxId =
+            state.analyses.length > 0
+              ? Math.max(...state.analyses.map((a) => a.id))
+              : 0;
+
+          newId = maxId + 1;
+
+          return {
+            analyses: [...state.analyses, { id: newId, ...analyse }],
+          };
+        });
+        return newId;
+      },
+
+      addPrediction: (id: number, predictions: Prediction[]) =>
+        set((state) => ({
+          analyses: state.analyses.map((analyse) =>
+            analyse.id === id
+              ? {
+                ...analyse,
+                prediction: [...(analyse.prediction || []), ...predictions],
+              }
+              : analyse
+          ),
+        })),
+    }),
     {
-      id: 2,
-      name: "Test2",
-      description: "",
-      created: new Date(2023, 5, 13),
-      files: [],
-    },
-    {
-      id: 3,
-      name: "Test3",
-      description: "gfdgsdfg",
-      created: new Date(2023, 4, 5),
-      files: [],
-    },
-  ],
-
-  addAnalyse: (analyse: Omit<Analyse, "id">) => {
-    let newId: number = 0; // Hacky. Find a better way to return the id.
-    set((state) => {
-      const maxId =
-        state.analyses.length > 0
-          ? Math.max(...state.analyses.map((a) => a.id))
-          : 0;
-
-      newId = maxId + 1;
-
-      return {
-        analyses: [...state.analyses, { id: newId, ...analyse }],
-      };
-    });
-    return newId;
-  },
-
-  addPrediction: (id: number, predictions: Prediction[]) =>
-    set((state) => ({
-      analyses: state.analyses.map((analyse) =>
-        analyse.id === id
-          ? {
-              ...analyse,
-              prediction: [...(analyse.prediction || []), ...predictions],
-            }
-          : analyse
-      ),
-    })),
-}));
+      name: 'application-store',
+      storage: createJSONStorage(() => sessionStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.analyses = state.analyses.map((analyse) => ({
+            ...analyse,
+            created: new Date(analyse.created),
+            files: analyse.files.map((file) => ({
+              ...file,
+              content: JSON.parse(JSON.stringify(file.content)), 
+              // Hmmm... This feels hacky. Find a better solutions that doesn't involve making it json again.
+              // But it's kinda the same with Date...
+            })),
+          }));
+        }
+      }
+    }
+  )
+);
 
 const useAnalyses = () => useApplicationStore((s) => s.analyses);
 const useGetAnalysesById = (id: number) => {
