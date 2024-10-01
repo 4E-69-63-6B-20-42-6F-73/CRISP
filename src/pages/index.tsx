9089d1a2f4119960a5f6f01a8ffb1fab02023ff7
Nick Bos <Nick.Bos@studiemaatje.company>
2024-10-01 19:06:49 +0200

    Add empty state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import {
-  Breadcrumb,
-  BreadcrumbButton,
-  BreadcrumbItem,
-  Button,
-  makeStyles,
-  tokens,
+    Breadcrumb,
+    BreadcrumbButton,
+    BreadcrumbItem,
+    Button,
+    makeStyles,
+    tokens,
 } from "@fluentui/react-components";
 import { AddFilled } from "@fluentui/react-icons";
 import { useNavigate } from "../router";
@@ -12,61 +12,59 @@ import { useAnalyses } from "../stores/ApplicationStore";
 import AnalyseDataGrid from "../components/index/analysesDatagrid";
 
 const useClasses = makeStyles({
-  div: {
-    display: "flex",
-    gap: "12px",
-  },
-  button: {
-    background: "linear-gradient(128.84deg,#0f6cbd 20.46%,#3c45ab 72.3%)", //TODO use tokens??
-    ":hover": {
-      background: "linear-gradient(128.84deg, #025caa 20.46%, #222b91 72.3%);",
+    div: {
+        display: "flex",
+        gap: "12px",
     },
-  },
-  datagrid: {
-    maxWidth: "1000px",
-    width: "-webkit-fill-available",
-    background: tokens.colorNeutralBackground1,
-    borderRadius: tokens.borderRadiusLarge,
-    padding: "12px",
-    marginTop: "12px",
-  },
+    button: {
+        background: "linear-gradient(128.84deg,#0f6cbd 20.46%,#3c45ab 72.3%)", //TODO use tokens??
+        ":hover": {
+            background: "linear-gradient(128.84deg, #025caa 20.46%, #222b91 72.3%);",
+        },
+    },
+    datagrid: {
+        maxWidth: "1000px",
+        width: "-webkit-fill-available",
+        background: tokens.colorNeutralBackground1,
+        borderRadius: tokens.borderRadiusLarge,
+        padding: "12px",
+        marginTop: "12px",
+    },
 });
 
 export default function Index() {
-  const classes = useClasses();
-  const navigate = useNavigate();
-  const analyses = useAnalyses();
+    const classes = useClasses();
+    const navigate = useNavigate();
+    const analyses = useAnalyses();
 
-  return (
-    <>
-      <div className={classes.div}>
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbButton current>My analyses</BreadcrumbButton>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <Button
-          className={classes.button}
-          appearance="primary"
-          shape="circular"
-          icon={<AddFilled />}
-          onClick={() => navigate("/new")}
-        >
-          {" "}
-          Create new
-        </Button>
-      </div>
+    return (
+        <>
+            <div className={classes.div}>
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <BreadcrumbButton current>My analyses</BreadcrumbButton>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+                <Button
+                    className={classes.button}
+                    appearance="primary"
+                    shape="circular"
+                    icon={<AddFilled />}
+                    onClick={() => navigate("/new")}>
+                    Create new
+                </Button>
+            </div>
 
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div className={classes.datagrid}>
-          <AnalyseDataGrid items={analyses} />
-        </div>
-      </div>
-    </>
-  );
+            {
+                analyses.length !== 0 ?
+                    <div className={classes.datagrid}>
+                        <AnalyseDataGrid items={analyses} />
+                    </div> :
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        <p>No analyses yet. Use Create new to create a new one!</p>
+                    </div>
+            }
+            
+        </>
+    );
 }
